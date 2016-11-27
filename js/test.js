527a18ec89b9a3995cb124158014e35cb5fbfb49
corentincaton <corentincaton@gmail.com>
2016-11-27 19:59:06 +0100

    JSONP issue solved on Chrome and Firefox

--- js/test.js
+++ js/test.js
@@ -1,61 +1,71 @@
+
 $( document ).ready(function() {
+    
+    $.ajax({    
         
-    //Get JSON data
-    var url='http://www.quiksilver.fr/s/QS-FR/dw/shop/v15_6/categories/root?levels=2&client_id=a76723ac-440f-4814-bd34-d492a140d78e'
-    $.getJSON(url, null, function(data){
-        
-        //Create and fill on array per category
-        var numberOfCategories = Object.keys(data.categories).length;
-        for(var i = 0; i< numberOfCategories;  i++) { 
-            
-            if(data.categories[i].c_showInMenu==1) {
+        // JSONP request
+        url: "http://www.quiksilver.fr/s/QS-FR/dw/shop/v15_6/categories/root?levels=2&client_id=a76723ac-440f-4814-bd34-d492a140d78e",
+        jsonp: "callback",
+        dataType: "jsonp",
+        data: {
+            format: "json"
+        },
 
-                //Create an empty table to be filled
-                var $table = $("<table></table>");
+        // Work with the response
+        success: function( response ) {
 
-                //Add category name as header
-                var $header = $("<th></th>").html(data.categories[i].name);
-                $table.append($header);
+            // Create and fill one array per category
+            var numberOfCategories = Object.keys(response.categories).length;
+            for(var i = 0; i< numberOfCategories;  i++) { 
 
-                //Add subcategories as rows
-                var $subCategories=data.categories[i].categories
-                if($subCategories != null) {
-                    var numberOfSubCategories=Object.keys($subCategories).length;
-                    for(var j=0; j<numberOfSubCategories; j++) {
-                        var $line = $("<tr></tr>").html($subCategories[j].name);
-                        $table.append($line);
-                    }
-                }   
-                $table.appendTo(document.body);            
+                if(response.categories[i].c_showInMenu==1) {
+
+                    // Create an empty table to be filled
+                    var $table = $("<table></table>");
+
+                    // Add category name as header
+                    var $header = $("<th></th>").html(response.categories[i].name);
+                    $table.append($header);
+
+                    // Add subcategories names as rows
+                    var $subCategories=response.categories[i].categories
+                    if($subCategories != null) {
+                        var numberOfSubCategories=Object.keys($subCategories).length;
+                        for(var j=0; j<numberOfSubCategories; j++) {
+                            var $line = $("<tr></tr>").html($subCategories[j].name);
+                            $table.append($line);
+                        }
+                    }   
+                    $table.appendTo(document.body);            
+                }
             }
+
+            // Wrap all rows in one div for each table
+            var $allTables = $('table');
+            for(var i=0; i<$allTables.length; i++) {
+                var $currentTable = $allTables[i];
+                $($currentTable.rows).wrapAll( "<div class=wrappedRows></div>" );
+            }
+
+            // Hide subcategories according to the screen size
+            $(window).resize(function(){
+                if($(window).width()>=640) 
+                {
+                    $('.wrappedRows').css('display','block');
+                }
+                else
+                {
+                    $('.wrappedRows').css('display','none');
+                }
+            });
+
+            // Slide down on click
+            $('table').click(function() {
+                if(this.children[1]!=null && $(window).width() < 640){
+                    $(this.children[1]).slideToggle();
+                }
+            });
         }
-        
-        //Wrap all rows in one div for each table
-        var $allTables = $('table');
-        for(var i=0; i<$allTables.length; i++) {
-            var $currentTable = $allTables[i];
-            $($currentTable.rows).wrapAll( "<div class=wrappedRows></div>" );
-        }
-        
-        //Handle subcategores hiding according to screen resizing
-        $(window).resize(function(){
-            if($(window).width()>=640) 
-            {
-                $('.wrappedRows').css('display','block');
-            }
-            else
-            {
-                $('.wrappedRows').css('display','none');
-            }
-        });
-        
-        //Slide down on click
-        $('table').click(function() {
-            if(this.children[1]!=null && $(window).width() < 640){
-                $(this.children[1]).slideToggle();
-            }
-        });
-    });      
+    });
 });
-
-
+    
